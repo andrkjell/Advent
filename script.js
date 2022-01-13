692385b358ebed987baaa680c54ed1f782cf6c50
andrkjell <97ankj96@gmail.com>
2022-01-13 12:27:30 +0100

    added forloop for making array

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 const input = fs.readFileSync('numbers.txt').toString(); //läser in txt filen med värderna angivna till mig
 const array = input.split(','); // använder mig an en split för att separera värderna givna i textfilen
+
+const fishPool = {};
+for (let i = 0; i < 9; i++) {
+fishPool[i] = 0;
+} //forloop för att skapa array med 8 platser
+
+console.log (fishPool);
