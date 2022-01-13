3c4326fc0d6f48c4e3e3cc193293916ed957bd7d
andrkjell <97ankj96@gmail.com>
2022-01-13 12:31:58 +0100

    added a counter for every number in the numbers.txt

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync('numbers.txt').toString(); //läser in txt filen med värderna angivna till mig
-const array = input.split(','); // använder mig an en split för att separera värderna givna i textfilen
+const valueCounter = input.split(','); // använder mig an en split för att separera värderna givna i textfilen
 
 const fishPool = {};
 for (let i = 0; i < 9; i++) {
 fishPool[i] = 0;
 } //forloop för att skapa array med 8 platser
+
+valueCounter.forEach(counter => {
+fishPool[counter]++;
+}); //räknar varje värde försig samt summerar dem
 
 console.log (fishPool);
